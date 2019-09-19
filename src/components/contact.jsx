ef9623006c7d665fcd85d98b18bf44edacab80ff
Josh Lopez <lopezja4@uci.edu>
2019-09-19 01:33:58 -0700

    Contact info update

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -5,11 +5,11 @@ class Contact extends Component {
   render() {
     return (
       <div className="contact page">
-        <h1 className="page-header">Get in touch!</h1>
+        <h1 className="page-header">Get in touch</h1>
         <div className="contact-info">
-          <p>Feel free to message me with any opportunites!</p>
-          <a href="mailto:josh@joshlopez.co">Email</a>
-          <a href="https://linkedin.com/in/josh-lopez1/">LinkedIn</a>
+          <div className="contact-entry">Email:&nbsp; <a href="mailto:josh@joshlopez.co">josh@joshlopez.co</a></div>
+          <div className="contact-entry">LinkedIn:&nbsp; <a href="https://linkedin.com/in/josh-lopez1/">josh-lopez-1</a></div>
+          <div className="contact-entry">Phone Number:&nbsp; <a href="/JoshLopezResume.pdf">See resume</a></div>
         </div>
       </div>
     );
